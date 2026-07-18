703a53e2eff837ef963ee400456a0c5034e8e94f
Prootzel <lirkel123@gmail.com>
2026-07-18 18:45:52 +0200

    Added 'library' for tower spawning

---
+++ Scripts/tower_generator.gd
@@ -0,0 +1,16 @@
+func create_tower(enemy_list : Array[Enemy], player_ref : Player):
+	# set up tower
+	pass	
+	
+	# spawn tower at random position
+
+func create_enemy_tower_config(enemy_list : Array[Enemy], player_ref : Player) -> EnemyTowerConfig:
+	var tc : EnemyTowerConfig
+
+	return tc
+	
+func create_enemy_tower() -> EnemyTower:
+	pass
+
+func generate_random_coordinates(tc : EnemyTowerConfig) -> Vector2:
+	pass
