3d1591c241842716e611663a0672dc3643b4968e
Max <138207893+MaxBluePanda@users.noreply.github.com>
2026-09-07 00:48:18 +0700

    Victory Screen!
    Added the victory() function to the game_over_menu script. Trigger it to show the victory screen! The victory screen has randomized subtitle that can be added or removed easily (since it's a public array of strings)

--- Scenes/ui/game_over_menu.gd
+++ Scenes/ui/game_over_menu.gd
@@ -4,7 +4,13 @@ class_name GameOverMenu
 signal play_again
 signal quit_game
 
+@onready var title: RichTextLabel = $MarginContainer/Panel/MarginContainer/VBoxContainer/TitleLabel
+@onready var subtitle: RichTextLabel = $MarginContainer/Panel/MarginContainer/VBoxContainer/SubtitleLabel
+
+@export_multiline var randomTexts: Array[String]
+
 func _ready() -> void:
+	subtitle.hide()
 	self.hide()
 
 func _unpause_game():
@@ -12,6 +18,15 @@ func _unpause_game():
 	get_tree().paused = false
 
 func game_over():
+	show_menu()
+
+func victory():
+	title.text = "VICTORY"
+	subtitle.show()
+	subtitle.text = randomTexts.pick_random()
+	show_menu()
+
+func show_menu():
 	if %MainMenu and %MainMenu.visible:
 		return
 	self.show()
