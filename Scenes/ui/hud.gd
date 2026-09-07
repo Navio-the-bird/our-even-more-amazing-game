8c7e466f41da7e48071ee7900ef00a17cc6706b0
Max <138207893+MaxBluePanda@users.noreply.github.com>
2026-09-07 22:07:22 +0700

    UI Stuff
    - Improve HUD
    - Make UI buttons for main menu and game over menu prettier

--- Scenes/ui/hud.gd
+++ Scenes/ui/hud.gd
@@ -4,11 +4,14 @@ var current_player_health:int = 20
 var max_player_health:int = 20
 var tower_count:int = 0
 
+func _init() -> void:
+	self.hide()
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	%HealthBar.value = current_player_health
 	%HealthBar.max_value = max_player_health
-	self.hide()
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -25,3 +28,17 @@ func set_player_max_health(value:int):
 func set_tower_count(value:int):
 	tower_count = value
 	%TowerCount.text = str(tower_count)
+
+func dim_mission_text():
+	%MissionText.show()
+	%MissionText.modulate = Color.WHITE
+	
+	await get_tree().create_timer(3.5).timeout
+	await create_tween().tween_property(%MissionText, "modulate", Color.TRANSPARENT, 2.0).set_trans(Tween.TRANS_SINE).finished
+	
+	%MissionText.hide()
+
+
+func _on_mission_text_visibility_changed() -> void:
+	if %MissionText.visible:
+		dim_mission_text()
--- Scenes/ui/main_menu/animated_button.gd
+++ Scenes/ui/main_menu/animated_button.gd
@@ -5,6 +5,9 @@ var hover_scale: Vector2 = Vector2(1.1, 1.1)
 var pressed_scale: Vector2 = Vector2(0.9, 0.9)
 var animation_duration: float = 0.1
 
+@export var normal_text_color: Color = Color.GRAY
+@export var hover_text_color: Color = Color.WHITE
+
 @export var hover_additional_text: String
 
 signal delayed_pressed
@@ -14,13 +17,18 @@ func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
 	pressed.connect(_on_pressed)
+	set("theme_override_colors/font_color", normal_text_color)
 
 func _on_mouse_entered() -> void:
 	create_tween().tween_property(self, "offset_transform_scale", hover_scale, animation_duration).set_trans(Tween.TRANS_SINE)
+	create_tween().tween_property(self, "theme_override_colors/font_color", hover_text_color, animation_duration)
+#	create_tween().tween_property(self, "offset_transform_scale", hover_scale, animation_duration).set_trans(Tween.TRANS_SINE)
 	text += hover_additional_text
 
 func _on_mouse_exited() -> void:
 	create_tween().tween_property(self, "offset_transform_scale", Vector2.ONE, animation_duration).set_trans(Tween.TRANS_SINE)
+	create_tween().tween_property(self, "theme_override_colors/font_color", normal_text_color, animation_duration)
+#	create_tween().tween_property(self, "theme_override_colors/font_outline_color", normal_text_color, animation_duration).set_trans(Tween.TRANS_SINE)
 	text = text.replace(hover_additional_text, "")
 
 func _on_pressed() -> void:
